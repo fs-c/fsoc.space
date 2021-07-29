e2fed588c3eee0d8ec47babb00b7b7bcf2b1ca0a
fsoc <root@fsoc.space>
2021-07-29 22:04:13 +0200

    Tweak header and footer styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,17 @@
 export const Header = ({ hero = false, title, className = '' }) => (<>
-    <header className={'container my-2' + className}>
+    <header className={`container ${hero ? 'my-4' : 'border-b border-gray-300 mt-4 mb-3 pb-1'}` + className}>
         {hero ? (
             <img src={'/assets/logo.svg'} alt={'fsoc'} className={'mx-auto'} />
         ) : (<>
-            <div className={'prose font-mono mt-4'}>
-                <a href={'/'}>fsoc</a>{title ? <>/<span className={'font-bold'}>{title}</span></> : <></>}
+            <div className={'prose'}>
+                <a href={'/'}>fsoc</a>{title ? <> / <span className={''}>{title}</span></> : <></>}
             </div>
         </>)}
     </header>
 </>);
 
 const Footer = () => (<>
-    <footer className={'flex flex-row gap-2 prose container text-sm py-2 border-t border-gray-300'}>
+    <footer className={'flex flex-row gap-2 prose container text-sm py-2 border-t border-gray-300 mt-4'}>
         <span>Built with <a href={'https://github.com/fs-c/dhow/tree/rewrite'}>fs-c/dhow</a>.</span>
 
         <div className={'flex-grow'} />
