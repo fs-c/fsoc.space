f3e4047f4e746f0ed040d6c160822b0a31a826e2
Laurenz Weixlbaumer <root@fsoc.space>
2022-09-24 00:52:37 +0200

    Adjust border color in dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import { Head } from '@fsoc/dhow';
 
 export const Header = ({ hero = false, title, href, className = '' }) => (<>
-    <header className={`container ${hero ? 'my-4' : 'border-b border-gray-300 mt-4 mb-4 pb-4'}` + className}>
+    <header className={`container ${hero ? 'my-4' : 'border-b border-gray-300 dark:border-gray-600 mt-4 mb-4 pb-4'}` + className}>
         {hero ? (
             <picture>
                 <source srcset={'/assets/logo-white.svg'} media={'(prefers-color-scheme: dark)'} />
@@ -16,7 +16,7 @@ export const Header = ({ hero = false, title, href, className = '' }) => (<>
 </>);
 
 const Footer = () => (<>
-    <footer className={'flex flex-row justify-between prose dark:prose-invert container text-md py-3 mb-1 border-t border-gray-300 mt-4'}>
+    <footer className={'flex flex-row justify-between prose dark:prose-invert container text-md py-3 mb-1 border-t border-gray-300 dark:border-gray-600 mt-4'}>
         <p className={'m-0'}>
             Built with <a href={'https://github.com/fs-c/dhow/tree/rewrite'}>fs-c/dhow</a>.
         </p>
